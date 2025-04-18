e2e705c28de8305dca4be01f60d916a8adabfcf0
Chandru <chandru89new@gmail.com>
2025-04-18 12:51:57 +0530

    Release v0.8.2
    - Move things around. Remove use of getConfig because we've switched to ReaderT.

--- src/Main.purs
+++ src/Main.purs
@@ -38,7 +38,7 @@ main = do
   cmd <- pure $ mkCommand args
   case cmd of
     Help -> log $ helpText
-    ShowVersion -> log $ "v0.8.1"
+    ShowVersion -> log $ "v0.8.2"
     Init -> launchAff_ $ do
       config <- getConfig
       res <- runAppM config initApp
