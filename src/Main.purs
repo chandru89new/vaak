8f64ebc98be704b4b6834d50ee0e264dd5db0a62
Chandru <chandru89new@gmail.com>
2025-04-19 00:57:57 +0530

    Release v0.8.3
    - Fixes a bug where a published post remained in output folder even after it was converted into a draft.

--- src/Main.purs
+++ src/Main.purs
@@ -41,7 +41,7 @@ main = do
   case cmd of
     Test -> test
     Help -> log $ helpText
-    ShowVersion -> log $ "v0.8.2"
+    ShowVersion -> log $ "v0.8.3"
     Init -> launchAff_ $ do
       config <- getConfig
       res <- runAppM config initApp
